c4e8644e40111ec949a256e370bc43954e52e4d7
Yoru <m.f.fetvaci@hotmail.com>
2024-07-22 16:46:56 +0300

    send message on random intervals

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -1,3 +1,6 @@
+use rand::rngs::OsRng;
+use tokio::time::Duration;
+
 use rand::Rng;
 use rusqlite::{params, Connection};
 
@@ -6,14 +9,47 @@ use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 
-use crate::utils::send_markov_message;
+use crate::utils::{generate_markov_message, get_most_popular_channel};
 
 pub struct Handler {}
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, _ctx: Context, bot: Ready) {
+    async fn ready(&self, ctx: Context, bot: Ready) {
         println!("Bot has started as {}", bot.user.name);
+
+        let mut rng = OsRng;
+        println!("started loop");
+        tokio::spawn(async move {
+            loop {
+                // Fetch vector of guilds the bot is in.
+                let guild_ids = ctx.cache.guilds();
+
+                for guild_id in guild_ids {
+                    let popular_channel_id = get_most_popular_channel(guild_id).await;
+                    let all_channels = ctx.http.get_channels(guild_id).await.unwrap();
+                    let channel_id = all_channels
+                        .iter()
+                        .find(|channel| channel.id.get() == popular_channel_id)
+                        .map(|channel| channel.id)
+                        .unwrap();
+
+                    let channel = ctx.http.get_channel(channel_id).await.unwrap();
+
+                    match channel.guild() {
+                        Some(channel) => {
+                            let builder = generate_markov_message(guild_id, channel.id).await;
+                            channel.send_message(&ctx.http, builder).await.unwrap();
+                        }
+                        None => {}
+                    }
+                }
+
+                // Wait a random second from 60 to 270
+                let range = rng.gen_range(60..270);
+                tokio::time::sleep(Duration::from_secs(range)).await;
+            }
+        });
     }
 
     async fn message(&self, ctx: Context, msg: Message) {
@@ -27,7 +63,11 @@ impl EventHandler for Handler {
         }
 
         if msg.mentions_me(&ctx.http).await.unwrap_or(false) {
-            send_markov_message(&ctx, &msg, guild_id).await;
+            let builder = generate_markov_message(guild_id, msg.channel_id).await;
+            msg.channel_id
+                .send_message(&ctx.http, builder)
+                .await
+                .unwrap();
             return;
         }
 
