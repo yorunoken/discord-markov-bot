c8bba9c886636b147e87f6101afbf80de21cc0c8
Yoru <m.f.fetvaci@hotmail.com>
2024-07-28 20:45:28 +0300

    finish greg command

--- src/commands/greg.rs
+++ src/commands/greg.rs
@@ -1,27 +1,35 @@
-use std::time::Instant;
-
 use serenity::all::CreateMessage;
-use serenity::builder::EditMessage;
 use serenity::model::channel::Message;
 use serenity::prelude::*;
 use serenity::Error;
 
+use crate::utils::generate_markov_message;
+
 pub async fn execute(
     ctx: &Context,
     msg: &Message,
-    _args: Vec<&str>,
+    args: Vec<&str>,
     _command_name: &String,
     _command_alias: Option<&str>,
 ) -> Result<(), Error> {
-    let timer_start = Instant::now();
+    let guild_id = match msg.guild_id {
+        Some(s) => s,
+        _ => return Ok(()),
+    };
 
-    let content = "Pong!";
-    let builder = CreateMessage::new().content(content);
-    let mut sent_message = msg.channel_id.send_message(&ctx.http, builder).await?;
+    let builder = match generate_markov_message(
+        guild_id,
+        msg.channel_id,
+        Some(args.join(" ").as_str()),
+    )
+    .await
+    {
+        Some(s) => s,
+        None => {
+            CreateMessage::new().content("Please wait until this channel has over 500 messages.")
+        }
+    };
 
-    let elapsed = (Instant::now() - timer_start).as_millis();
-
-    let builder = EditMessage::new().content(format!("{} ({}ms)", content, elapsed));
-    sent_message.edit(&ctx.http, builder).await?;
+    msg.channel_id.send_message(&ctx.http, builder).await?;
     Ok(())
 }
