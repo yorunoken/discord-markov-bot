a455b2ee035a747c99f594b95d08f520cbecae31
Yoru <m.f.fetvaci@hotmail.com>
2024-10-08 23:02:25 +0300

    wait for connection

--- src/commands/leaderboard.rs
+++ src/commands/leaderboard.rs
@@ -66,7 +66,18 @@ pub async fn execute(
     ];
 
     let embed = tokio::task::spawn_blocking(move || {
-        let conn = Connection::open("messages.db").expect("Unable to open database");
+        let mut conn: Option<Connection> = None;
+        for i in 0..=5 {
+            match Connection::open("messages.db") {
+                Ok(conn_ok) => conn = Some(conn_ok),
+                Err(err) => {
+                    eprintln!("Errored while opening db: {}, i: {}", err, i);
+                    std::thread::sleep(std::time::Duration::from_secs(5));
+                }
+            };
+        }
+
+        let conn = conn.expect("Failed to establish database connection after multiple attempts.");
 
         let mut query = String::from("SELECT content, author_id FROM messages WHERE guild_id = ?1");
 
--- src/utils.rs
+++ src/utils.rs
@@ -19,7 +19,18 @@ pub async fn generate_markov_message(
     const DATABASE_MESSAGE_FETCH_LIMIT: usize = 2000;
 
     let sentences: Vec<String> = tokio::task::spawn_blocking(move || {
-        let conn = Connection::open("messages.db").expect("Unable to open database");
+        let mut conn: Option<Connection> = None;
+        for i in 0..=5 {
+            match Connection::open("messages.db") {
+                Ok(conn_ok) => conn = Some(conn_ok),
+                Err(err) => {
+                    eprintln!("Errored while opening db: {}, i: {}", err, i);
+                    std::thread::sleep(std::time::Duration::from_secs(5));
+                }
+            };
+        }
+
+        let conn = conn.expect("Failed to establish database connection after multiple attempts.");
 
         let mut stmt = conn
             .prepare(
